a6a1e496623a3f7e063facb65454e3c86241e25a
Shavkat <shavkatk06@gmail.com>
2024-12-10 12:03:38 +0300

    9th commit

--- src/routes/Photos/PhotosPage.jsx
+++ src/routes/Photos/PhotosPage.jsx
@@ -2,7 +2,14 @@ import s from "./PhotosPage.module.css"
 
 export const PhotosPage = () => {
   return (
-    <div className={s.btnCol}><h2 >Перейти к колекции</h2></div>
+    <div className={s.photosmaindiv}>
+      <div>
+        <img className={s.photos} src="./photos/PhotosPagePhoto1.png" alt="" />
+        <img className={s.photos} src="./photos/PhotosPagePhoto2.png" alt="" />
+        <img className={s.photos} src="./photos/PhotosPagePhoto3.png" alt="" />
+      </div>
+      <h2 className={s.title}>Горные озёра</h2>
+      <button className={s.btnCol}>Перейти к колекции</button>
+    </div>
   )
 };
-
